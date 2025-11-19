f8a0644e96cb70a23c98e5042dae2d3365700422
Xavier Canjura <fernanxavi58@gmail.com>
2025-11-18 22:53:21 -0600

    chore: fix webpack for prod

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,11 +8,12 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'contianer',
+      name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
